0f9c58c7b882e9d24c5b9fe5da2912980be0c749
nanakenashi <nanakenashi@gmail.com>
2016-10-06 21:15:41 +0900

    Added some files

---
+++ example.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_eip" "ip" {
+    instance = "${aws_instance.example.id}"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-ba3e14d9"
+  instance_type = "t2.micro"
+}
